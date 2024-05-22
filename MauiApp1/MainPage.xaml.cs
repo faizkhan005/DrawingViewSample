abef73cb554167c99d2626b85626c3a29d4ce00c
Faizan Khan <faizan.khan@epicor.com>
2024-05-22 10:36:10 +0530

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Maui.Core;
+using System.Collections.ObjectModel;
+
+namespace MauiApp1
+{
+    public partial class MainPage : TabbedPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public ObservableCollection<IDrawingLine> Lines { get; set; }
+        
+
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Signature.Lines.Clear();
+        }
+    }
+
+}
